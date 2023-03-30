195d76745ab2be444284e2ab76ac37731f8ad36a
icliniq <ashik.a@icliniq.com>
2023-03-30 18:01:36 +0530

    Updated Cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
+ 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: cloudflare({ mode: "advanced" })
+
 });
